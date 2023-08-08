386f01965ffcdcc0e6d0c1dfcedfb1bd5f64c678
swetatmkr <tamrakar5525@gmail.com>
2023-08-08 22:34:12 +0545

    all

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
 
 require_once "./connection.php";
-if(isset($_POST['login'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-
-$sql = "SELECT * FROM  users where email = ? AND password = ?";
-$statement = $pdo->prepare($sql);
-$statement->execute([
-    $email,
-     $password
-]);
-$result = $statement->fetch();
-
-if(!empty($result)){
-    header("Location: page.php");
-}
-else{
-    
-        $errors['email'] = "Email incorrect";
-        $errors['password']= "Password incorrect";
-    }
-}
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +10,7 @@ else{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sweet Cake</title>
+    <title>Cute Cats</title>
 
     <!-- swiper link  -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
@@ -52,7 +27,7 @@ else{
     <!-- header section start here  -->
     <header class="header">
         <div class="logoContent">
-            <a href="#" class="logo"><img src="logo.png" alt=""></a>
+            <a href="#" class="logo"><img src="image/logo.png" alt=""></a>
             <h1 class="logoName">Fur Babies </h1>
         </div>
 
@@ -60,51 +35,15 @@ else{
             <a href="#home">home</a>
             <a href="#product">product</a>
             <a href="#blogs">blogs</a>
-            <a href="#review">review</a>
             <a href="#contact">contact</a>
         </nav>
 
         <div class="icon">
             <i class="fas fa-bars" id="menu"></i>
-            <i class="fas fa-user" id="user"></i>
+         <a href="register.php"><i class="fas fa-user" id="user" ></i> </a> 
         </div>
-        <div class="container w-1/2 mt-12">
-            <!--<p class="text-center uppercase text-red-500 font-bold text-sm">Welcome!</p>-->
-            <h1 class="text-center text-3xl font-bold my-2">Login Now</h1>
-        
-            <form method= "post" class="form-container bg-white p-8 mt-4 shadow-md">
-                <?php if(!empty($errors)){ ?>
-               <div class="errors bg-red-200 text-red-700 p-4 my-4">
-                <?php foreach($errors as $key => $value){     ?>
-                    <li><?php echo $value; ?> </li>
-                    <?php } ?>
-                </div>
-                <?php } ?>
-            
-                    
-                    <div class="row flex mt-2">
-                    <div class="col w-full">
-                        <label >Email Address</label>
-                        <input type="text" class="block border-2 border-gray-300 w-full px-4 py-1 mt-2" name="email">   
-                </div>
-        
-                
-                </div>
-        
-                <div class="row flex mt-2">
-                    <div class="col w-full">
-                        <label >Password</label>
-                        <input type="password" class="block border-2 border-gray-300 w-full px-4 py-1 mt-2" name="password">   
-                </div>
-        
-                
-                </div>
-        
-                <div class="text-center mt-4">
-                        <button class="bg-red-500 px-12 py-2 text-white mt-2 uppercase hover:bg-red-700" name="login">Login</button>
-                    </div>
-                </div>
-            </form>
+    
+       
     </header>
     <!-- header section end here  -->
 
@@ -112,8 +51,7 @@ else{
     <section class="home" id="home">
         <div class="homeContent">
             <h2>Cute Cats for everyone! </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum excepturi doloribus, fuga quod deserunt
-                recusandae?</p>
+            <p>Who doesnt love cats? Come on a journey with us to see some cats and brighten our day</p>
             <div class="home-btn">
                 <a href="#"><button>see more</button></a>
             </div>
@@ -131,7 +69,7 @@ else{
             <div class="swiper-wrapper">
                 <div class="swiper-slide box">
                     <div class="img">
-                        <img src="cat1.png" alt="">
+                       <a href="second/random.html"> <img src="image/cat1.png" alt="cat"></a>
                     </div>
                     <div class="product-content">
                         <h3>Random Cat Pictures</h3>
@@ -140,7 +78,7 @@ else{
                 </div>
                 <div class="swiper-slide box">
                     <div class="img">
-                        <img src="cat2.png" alt="">
+                       <a href="second/breed.html"> <img src="image/cat2.png" alt="cat"> </a> 
                     </div>
                     <div class="product-content">
                         <h3>Different Breeds</h3>
@@ -149,7 +87,7 @@ else{
                 </div>
                 <div class="swiper-slide box">
                     <div class="img">
-                        <img src="cat3.png" alt="">
+                       <a href="second/vote.html"> <img src="image/cat3.png" alt="cat"></a>
                     </div>
                     <div class="product-content">
                         <h3>Vote your favourite</h3>
@@ -168,7 +106,7 @@ else{
             <div class="swiper-wrapper">
                 <div class=" swiper-slide box">
                     <div class="img">
-                        <img src="cat4.png" alt="">
+                        <img src="image/cat4.png" alt="">
                     </div>
                     <div class="content">
                         <h3>Some Interesting Facts!</h3>
@@ -188,12 +126,6 @@ else{
 
     <!-- blogs section ends here  -->
 
-    <!-- newsletter section start here  -->
-
-    
-    <!-- newsletter section ends here  -->
-
-    <!-- review section start here  -->
     
 
     <!-- footer section start here  -->
@@ -201,7 +133,7 @@ else{
     <footer class="footer" id="contact">
         <div class="box-container">
             <div class="mainBox">
-              >
+              
            
         <div class="share">
             <a href="#" class="fab fa-facebook-f"></a>
